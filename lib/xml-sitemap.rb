82a135729b4e4398c5a29908bbdc9e0c5710e8a1
Dan Sosedoff <dan.sosedoff@gmail.com>
2010-06-18 01:04:32 -0500

    initial commit

---
+++ lib/xml-sitemap.rb
@@ -0,0 +1,58 @@
+module XmlSitemap
+  PERIODS = [:none, :always, :hourly, :daily, :weekly, :monthly, :yearly, :never]
+
+  class Item
+    attr_reader :path
+    attr_reader :updated
+    attr_reader :priority
+    attr_reader :changefreq
+    
+    def initialize(opts={})
+      @path = opts[:url] if opts.key?(:url)
+      @updated = opts[:updated] || Time.now
+      @priority = opts[:priority] || 0.5
+      @changefreq = opts[:period] || :never
+    end
+  end
+
+  class Map
+    attr_reader :domain, :items
+    attr_reader :buffer
+    
+    # Creates new Map class for specified domain
+    def initialize(domain)
+      @domain = domain
+      @items = []
+      yield self if block_given?
+    end
+    
+    # Yields Map class for easier access
+    def generate
+      raise ArgumentError, 'Block required' unless block_given?
+      yield self
+    end
+    
+    # Add new item to sitemap list
+    def add(opts)
+      @items << XmlSitemap::Item.new(opts)
+    end
+    
+    # Render XML
+    def render
+      return '' if @items.size == 0
+      output = []
+      output << '<?xml version="1.0" encoding="UTF-8"?>'
+      output << '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+      @items.each do |item|
+        output << '<url>'
+          output << "<loc>http://#{@domain}#{item.path.to_s}</loc>"
+          output << "<lastmod>#{item.updated.to_date}</lastmod>"
+          output << "<changefreq>#{item.changefreq.to_s}</changefreq>"
+          output << "<priority>#{item.priority.to_s}</priority>"
+        output << '</url>'
+      end
+      output << '</urlset>'
+      return output.join("\n")
+    end
+  end
+end
---
+++ xml-sitemap.gemspec
@@ -0,0 +1,20 @@
+Gem::Specification.new do |s|
+  s.name = 'xml-sitemap'
+  s.version = '1.0.1'
+  s.date = '2010-06-18'
+  s.description = "Easy XML sitemap generation for Ruby/Rails application"
+  s.summary = "No-dependency XML sitemap generator. Can be used for any Ruby application (Rails/Merb/Sinatra)."
+  s.authors = ["Dan Sosedoff"]
+  s.email = "dan.sosedoff@gmail.com"
+
+  # = MANIFEST =
+  s.files = %w[
+    README
+    lib/xml-sitemap.rb
+  ]
+  # = MANIFEST =
+
+  s.has_rdoc = true
+  s.homepage = "http://sosedoff.com/"
+  s.rubygems_version = '1.3.5'
+end
