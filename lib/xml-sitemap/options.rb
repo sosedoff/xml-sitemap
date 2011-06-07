874c08de3e7eb8f984785d5fc5adc0a28ad4f42f
Dan Sosedoff <dan.sosedoff@gmail.com>
2011-06-07 17:13:54 -0500

    modifications for ruby 1.8/ree tests

--- lib/xml-sitemap/options.rb
+++ lib/xml-sitemap/options.rb
@@ -11,9 +11,9 @@ module XmlSitemap
   ].freeze
   
   MAP_SCHEMA_OPTIONS = {
-    'xmlns'              => "http://www.sitemaps.org/schemas/sitemap/0.9",
+    'xsi:schemaLocation' => "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd",
     'xmlns:xsi'          => "http://www.w3.org/2001/XMLSchema-instance",
-    'xsi:schemaLocation' => "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+    'xmlns'              => "http://www.sitemaps.org/schemas/sitemap/0.9"
   }.freeze
   
   INDEX_SCHEMA_OPTIONS = {
