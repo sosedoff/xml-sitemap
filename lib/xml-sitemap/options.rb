93bbc83857fc9aaedd694d553091d752bcbceb77
Dan Sosedoff <dan.sosedoff@gmail.com>
2011-10-22 20:04:10 -0500

    Fixed formatting

--- lib/xml-sitemap/options.rb
+++ lib/xml-sitemap/options.rb
@@ -19,6 +19,6 @@ module XmlSitemap
   INDEX_SCHEMA_OPTIONS = {
     'xsi:schemaLocation' => "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd",
     'xmlns:xsi'          => "http://www.w3.org/2001/XMLSchema-instance",
-    'xmlns' => "http://www.sitemaps.org/schemas/sitemap/0.9"
+    'xmlns'              => "http://www.sitemaps.org/schemas/sitemap/0.9"
   }.freeze
 end
