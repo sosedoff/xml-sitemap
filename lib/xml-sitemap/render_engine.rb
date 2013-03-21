0c675d82a8121d0e829c28ff0e45588fa0ac35d8
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-03-20 23:02:20 -0500

    Use string append operator

--- lib/xml-sitemap/render_engine.rb
+++ lib/xml-sitemap/render_engine.rb
@@ -70,36 +70,38 @@ module XmlSitemap
       result = '<?xml version="1.0" encoding="UTF-8"?>' + "\n<urlset"
 
       XmlSitemap::MAP_SCHEMA_OPTIONS.each do |key, val|
-        result += ' ' + key + '="' + val + '"'
+        result << ' ' + key + '="' + val + '"'
       end
 
-      result += ">\n"
+      result << ">\n"
 
       item_results = []
       @items.each do |item|
         item_string  = "  <url>\n"
-        item_string += "    <loc>#{CGI::escapeHTML(item.target)}</loc>\n"
+        item_string << "    <loc>#{CGI::escapeHTML(item.target)}</loc>\n"
 
         # Format and image tag specifications found in http://support.google.com/webmasters/bin/answer.py?hl=en&answer=178636
         if item.image_location
-          item_string += "    <image:image>\n"
-          item_string += "      <image:loc>#{CGI::escapeHTML(item.image_location)}</image:loc>\n"
-          item_string += "      <image:caption>#{CGI::escapeHTML(item.image_caption)}</image:caption>\n"               if item.image_caption
-          item_string += "      <image:title>#{CGI::escapeHTML(item.image_title)}</image:title>\n"                     if item.image_title
-          item_string += "      <image:license>#{CGI::escapeHTML(item.image_license)}</image:license>\n"               if item.image_license
-          item_string += "      <image:geo_location>#{CGI::escapeHTML(item.image_geolocation)}</image:geo_location>\n" if item.image_geolocation
-          item_string += "    </image:image>\n"
+          item_string << "    <image:image>\n"
+          item_string << "      <image:loc>#{CGI::escapeHTML(item.image_location)}</image:loc>\n"
+          item_string << "      <image:caption>#{CGI::escapeHTML(item.image_caption)}</image:caption>\n"               if item.image_caption
+          item_string << "      <image:title>#{CGI::escapeHTML(item.image_title)}</image:title>\n"                     if item.image_title
+          item_string << "      <image:license>#{CGI::escapeHTML(item.image_license)}</image:license>\n"               if item.image_license
+          item_string << "      <image:geo_location>#{CGI::escapeHTML(item.image_geolocation)}</image:geo_location>\n" if item.image_geolocation
+          item_string << "    </image:image>\n"
         end
 
-        item_string += "    <lastmod>#{item.lastmod_value}</lastmod>\n"
-        item_string += "    <changefreq>#{item.changefreq}</changefreq>\n"
-        item_string += "    <priority>#{item.priority}</priority>\n"
-        item_string += "  </url>\n"
+        item_string << "    <lastmod>#{item.lastmod_value}</lastmod>\n"
+        item_string << "    <changefreq>#{item.changefreq}</changefreq>\n"
+        item_string << "    <priority>#{item.priority}</priority>\n"
+        item_string << "  </url>\n"
 
         item_results << item_string
       end
 
-      result = result + item_results.join("") + "</urlset>\n"
+      result << item_results.join("")
+      result << "</urlset>\n"
+      
       result
     end
   end
